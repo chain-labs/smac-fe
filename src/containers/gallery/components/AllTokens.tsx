51e0ffa3d936ea2120d596d61e89333956afebde
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2021-12-04 20:38:13 +0530

    solving scrollbar issue and making images dynamic

--- src/containers/gallery/components/AllTokens.tsx
+++ src/containers/gallery/components/AllTokens.tsx
@@ -1,79 +1,63 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import HorizontalScroll from "react-scroll-horizontal";
-import Box from 'src/components/Box';
-
+import Box from "src/components/Box";
+import Image from "next/image";
 
 const AllTokens = () => {
-    const [left, setLeft] = useState<string>("21rem");
-    return (
-        <Box position="relative" top="5rem" height="100%" ml={left} >
-				<HorizontalScroll
-					pageLock={false}
-					reverseScroll={true}
-					// style         = { {position:"relative",top:"50rem"} }
-					// config        = {{ stiffness: int, damping: int }}
-					// className     = { string }
-					// animValues    = { int }
-				>
-                    <Box>
-                        <Box display="flex" justifyContent="space-around" mb="2.4rem">
-                    {
-                        // arr?.map(c=>{
-                        //     <Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-
-                        // })
-                    }
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-                            <Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-                            <Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-
+	const [arr, setArr] = useState([...Array(10)].map((_, i) => i + 1));
+	const [left, setLeft] = useState<string>("21rem");
+	return (
+		<Box top="5rem" ml={left} overflow="hidden" hideScrollBar>
+			<Box
+				id="scroll"
+				overflowX="scroll"
+				hideScrollBar
+				onScroll={() => {
+					setLeft("0");
+				}}
+			>
+				<Box display="flex" justifyContent="space-around" mb="2.4rem">
+					{arr.map((_, i) => (
+						<Box 
+						// width="19.6rem" height="19.6rem"
+						>
+							{/* <Image
+								src={`/static/images/Nft-${arr[i]}.png`}
+								layout="fill"
+								/> */}
+							<Box
+								as="img"
+								src={`/static/images/Nft-${arr[i]}.png`}
+								height="150px"
+								width="150px"
+								mr="12px"
+							></Box>
+							<Box
+								as="img"
+								src={`/static/images/Nft-${arr[i]}.png`}
+								height="150px"
+								width="150px"
+								mr="12px"
+							></Box>
 						</Box>
-						<Box display="flex" justifyContent="space-around" mb="2.4rem">
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-                            <Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-                            <Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-
-						</Box>
-						<Box display="flex" justifyContent="space-around" mb="2.4rem">
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-                            <Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-                            <Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft1.png" height="196px" width="196px" mr="12px"></Box>
-
-						</Box>
-					</Box>
-				</HorizontalScroll>
+					))}
+					{/* <Box as="img" src="/static/images/Nft-1.png" height="196px" width="196px" mr="12px"></Box>
+                            <Box as="img" src="/static/images/Nft-1.png" height="196px" width="196px" mr="12px"></Box>
+							<Box as="img" src="/static/images/Nft-1.png" height="196px" width="196px" mr="12px"></Box>
+							<Box as="img" src="/static/images/Nft-1.png" height="196px" width="196px" mr="12px"></Box>
+							<Box as="img" src="/static/images/Nft-1.png" height="196px" width="196px" mr="12px"></Box>
+							<Box as="img" src="/static/images/Nft-1.png" height="196px" width="196px" mr="12px"></Box>
+							<Box as="img" src="/static/images/Nft-1.png" height="196px" width="196px" mr="12px"></Box>
+							<Box as="img" src="/static/images/Nft-1.png" height="196px" width="196px" mr="12px"></Box>
+							<Box as="img" src="/static/images/Nft-1.png" height="196px" width="196px" mr="12px"></Box>
+							<Box as="img" src="/static/images/Nft-1.png" height="196px" width="196px" mr="12px"></Box>
+                            <Box as="img" src="/static/images/Nft-1.png" height="196px" width="196px" mr="12px"></Box>
+							<Box as="img" src="/static/images/Nft-1.png" height="196px" width="196px" mr="12px"></Box>
+							<Box as="img" src="/static/images/Nft-1.png" height="196px" width="196px" mr="12px"></Box> */}
+				</Box>
 			</Box>
-    )
-}
+		</Box>
+	);
+};
 
-export default AllTokens
+export default AllTokens;
