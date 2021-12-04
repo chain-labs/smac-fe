ac312f338933b17e33dafd04af9a48cc8c8f7fb7
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2021-12-04 22:29:33 +0530

    page done only scrollbar issue remaining to solve

--- src/containers/gallery/components/AllTokens.tsx
+++ src/containers/gallery/components/AllTokens.tsx
@@ -5,55 +5,49 @@ import Image from "next/image";
 
 const AllTokens = () => {
 	const [arr, setArr] = useState([...Array(10)].map((_, i) => i + 1));
-	const [left, setLeft] = useState<string>("21rem");
+	const [left, setLeft] = useState<string>("23rem");
 	return (
-		<Box top="5rem" ml={left} overflow="hidden" hideScrollBar>
+		<Box top="5rem" ml={left} overflow="hidden">
 			<Box
 				id="scroll"
 				overflowX="scroll"
-				hideScrollBar
 				onScroll={() => {
 					setLeft("0");
 				}}
+				css={`
+				&::-webkit-scrollbar-track:{
+					display:none
+				}
+				`}
 			>
 				<Box display="flex" justifyContent="space-around" mb="2.4rem">
 					{arr.map((_, i) => (
-						<Box 
-						// width="19.6rem" height="19.6rem"
-						>
-							{/* <Image
-								src={`/static/images/Nft-${arr[i]}.png`}
-								layout="fill"
-								/> */}
-							<Box
-								as="img"
-								src={`/static/images/Nft-${arr[i]}.png`}
-								height="150px"
-								width="150px"
-								mr="12px"
-							></Box>
-							<Box
-								as="img"
-								src={`/static/images/Nft-${arr[i]}.png`}
-								height="150px"
-								width="150px"
-								mr="12px"
-							></Box>
+						<Box mt="3rem">
+							<Box mb="2rem"
+								mr="mxl"
+								position="relative"
+								height={{ tabS: "12rem", deskL: "21rem" }}
+								width={{ tabS: "12rem", deskL: "21rem" }}
+							>
+								<Image src={`/static/images/Nft-${arr[i]}.png`} layout="fill" />
+							</Box>
+							<Box mb="2rem"
+								mr="mxl"
+								position="relative"
+								height={{ tabS: "12rem", deskL: "21rem" }}
+								width={{ tabS: "12rem", deskL: "21rem" }}
+							>
+								<Image src={`/static/images/Nft-${arr[i]}.png`} layout="fill" />
+							</Box><Box
+								mr="mxl"
+								position="relative"
+								height={{ tabS: "12rem", deskL: "21rem" }}
+								width={{ tabS: "12rem", deskL: "21rem" }}
+							>
+								<Image src={`/static/images/Nft-${arr[i]}.png`} layout="fill" />
+							</Box>
 						</Box>
 					))}
-					{/* <Box as="img" src="/static/images/Nft-1.png" height="196px" width="196px" mr="12px"></Box>
-                            <Box as="img" src="/static/images/Nft-1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft-1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft-1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft-1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft-1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft-1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft-1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft-1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft-1.png" height="196px" width="196px" mr="12px"></Box>
-                            <Box as="img" src="/static/images/Nft-1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft-1.png" height="196px" width="196px" mr="12px"></Box>
-							<Box as="img" src="/static/images/Nft-1.png" height="196px" width="196px" mr="12px"></Box> */}
 				</Box>
 			</Box>
 		</Box>
