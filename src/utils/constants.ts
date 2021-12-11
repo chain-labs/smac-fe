ccf7269233337ae806ac753da1b5875e589d8e7a
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2021-12-12 04:21:37 +0530

    Revert "Merge branch '29-buy-feature' of https://github.com/chain-labs/smac-fe into 13-gallerypage"
    This reverts commit e2a9fc654b18ac7f73cc0b1d68b8d6928ef34a6f, reversing
    changes made to 1372f085eeca825cf12900cb85992685127f503f.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,11 +4,7 @@ export const PINATA_KEY_SECRET = process.env.NEXT_PUBLIC_IPFS_PINATA_API_SECRET;
 export const PINATA_URL = "https://api.pinata.cloud/";
 
 export const CONTRACT_POLYGON_ADDRESS =
-  process.env.NEXT_PUBLIC_CONTRACT_ADDRESS;
-
-// export const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+  "0xdA9f0476EC00994ebfF2C55cDf1a4D4d23f14d02";
 
 export const CONTRACT_ABI_URL =
   "https://api.polygonscan.com/api?module=contract&action=getabi&address=0x8F0C4A7044188a59eb89e6bdE8f27e2f8E0A46d5&apikey=P7DI6NDJDNR5EXYEAR4NPI8EJNQH2HXSME";
-
-// export const CONTRACT_ABI_URL = `https://api.polygonscan.com/api?module=contract&action=getabi&address=${CONTRACT_POLYGON_ADDRESS}&apikey=${API_KEY}`;
