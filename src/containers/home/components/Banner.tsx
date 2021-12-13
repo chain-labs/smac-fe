0a20c601ae117122097fbb303069ed91e8861e6c
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2021-12-13 12:03:01 +0530

    Revert "Revert "Revert "Merge branch '29-buy-feature' of https://github.com/chain-labs/smac-fe into 13-gallerypage"""
    This reverts commit 39f675415d3f277c86364c6fcc35601fd8c9d35b.

--- src/containers/home/components/Banner.tsx
+++ src/containers/home/components/Banner.tsx
@@ -14,34 +14,23 @@ const Banner = memo(() => {
       className="banner"
       key="banner"
     >
-      <Box
-        height={{ mobS: "112.5vw", tabS: "56.25vw" }}
-        width={{ mobS: "200vw", tabS: "100vw" }}
-        position="relative"
-        ml={{ mobS: "-20rem", tabS: "0", tabL: "0", deskL: "0" }}
-        mt={{ mobS: "5rem", tabS: "10rem", tabL: "0", deskL: "0" }}
-        transform={{
-          mobS: "scale(1.5)",
-          tabS: "scale(1.2)",
-          deskL: "scale(1)",
+      <Image
+        id="banner-image"
+        src="/static/images/banner.webp"
+        alt="banner"
+        height="9"
+        width="16"
+        layout="responsive"
+        quality={10}
+        priority
+        onLoadingComplete={() => {
+          scrollBannerAnimation();
+          introAnimation();
         }}
-      >
-        <Image
-          id="banner-image"
-          src="/static/images/banner.webp"
-          alt="banner"
-          layout="fill"
-          quality={10}
-          priority
-          onLoadingComplete={() => {
-            scrollBannerAnimation();
-            introAnimation();
-          }}
-        ></Image>
-      </Box>
+      ></Image>
       <Box
         bg="black-10"
-        opacity="80%"
+        opacity="70%"
         height="120vh"
         width="100vw"
         position="absolute"
